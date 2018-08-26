645abce0602c5195ceae10f1142b041935f11ab0
DevHyung <khuphj@gmail.com>
2018-08-27 02:54:14 +0900

    엑셀추가부분작성

--- parser.py
+++ parser.py
@@ -26,6 +26,7 @@ def get_cookie_str(_cookie):
     return  cookieStr
 
 def get_info():
+    depth2List = []
     for pageIdx in range(1,lastPage+1):
         log('s',"{} 페이지 추출중...".format(pageIdx))
         html = requests.get(url.format(get_url_encode(keyword), pageIdx), headers=headers)
@@ -47,11 +48,15 @@ def get_info():
                 tel = result['tel']
                 code = result['id'][1:]
                 homepage = result['homePage']
-                print(name)
-                #print([name, roadAddress, address, tel, homepage, detailBaseUrl + code])
+                depth2List.append([name, roadAddress, address, tel, homepage, detailBaseUrl + code])
         else:
             log('s',"끝")
-        time.sleep(random.randint(3,7))
+
+        if len(depth2List) >= 50:
+            save_excel(FILENAME, depth2List, None)  # init
+            depth2List.clear()
+        time.sleep(random.randint(MIN,MAX))
+    save_excel(FILENAME, depth2List, None)  # init
 
 def log(tag, text):
 	# Info tag
@@ -69,8 +74,10 @@ def README():
     print("# 사용법 :")
     print("# 1. 저장 파일의 이름만 적는다 (기본 포맷은 xlsx 이며 확장자는 적지 않는다 ) ")
     print("#     1-1. 위에 적은 파일은 프로그램이 종료전까지 계속 데이터가 쌓인다")
-    print("# 2. 검색할 키워드를 입력 한다. ")
-    print("#     2-1. 끝내시려면 숫자 0을 입력하시고 엔터를 누르시면 됩니다.")
+    print("# 2. 페이지 -> 페이지 파싱간의 딜레이 최소, 최대 정수값을 입력한다. ")
+    print("#     2-1. 예를들어 3,10을 입력하면 페이지간 딜레이가 3~10사이의 랜덤정수가 된다.")
+    print("# 3. 검색할 키워드를 입력 한다. ")
+    print("#     3-1. 끝내시려면 숫자 0을 입력하시고 엔터를 누르시면 됩니다.")
     print("#_____________________________________________________________________")
 
 def save_excel(_FILENAME, _DATA, _HEADER):
@@ -107,9 +114,11 @@ if __name__ == "__main__":
     driver.get('https://map.naver.com/')
     # get cookie
     FILENAME = input(">>> 저장 파일 *이름만* 적어주세요 ( 확장자 미포함 ) :") + '.xlsx'
+    MIN = int ( input(">>> 페이지 딜레이 최소 정수값 : ") )
+    MAX = int ( input(">>> 페이지 딜레이 최대 정수값 : ") )
     HEADER = ['키워드','업소명', '도로명주소', '지번주소', '전화번호', '홈페이지1', '상세페이지']
     save_excel(FILENAME,None,HEADER) # init
-    
+
     while True:
         keyword = input(">>> 검색하실 키워드를 입력(끝내려면 0 입력) : ").strip()
         if keyword == '0':
