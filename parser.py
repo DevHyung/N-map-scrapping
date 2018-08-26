a6d5a2c5f4fbd7e8194e748b6e01e42221b7d1b2
DevHyung <khuphj@gmail.com>
2018-08-27 02:45:25 +0900

    엑셀모듈추가

--- parser.py
+++ parser.py
@@ -7,8 +7,8 @@ from bs4 import BeautifulSoup
 import time
 import random
 from math import ceil
-
-
+from openpyxl import load_workbook,Workbook
+import os
 def get_url_encode(_query):
     return quote(_query).replace('%20', '+')
 
@@ -64,16 +64,55 @@ def log(tag, text):
 	elif(tag == 's'):
 		print("[SUCCESS] " + text)
 
+def README():
+    print("_____ Copyright @Hyung June, Park ____________________________________")
+    print("# 사용법 :")
+    print("# 1. 저장 파일의 이름만 적는다 (기본 포맷은 xlsx 이며 확장자는 적지 않는다 ) ")
+    print("#     1-1. 위에 적은 파일은 프로그램이 종료전까지 계속 데이터가 쌓인다")
+    print("# 2. 검색할 키워드를 입력 한다. ")
+    print("#     2-1. 끝내시려면 숫자 0을 입력하시고 엔터를 누르시면 됩니다.")
+    print("#_____________________________________________________________________")
+
+def save_excel(_FILENAME, _DATA, _HEADER):
+    if os.path.exists(_FILENAME):
+        if _DATA == None:
+            return None
+        book = load_workbook(_FILENAME)
+        sheet = book.active
+        for depth1List in _DATA:
+            sheet.append(depth1List)
+        book.save(_FILENAME)
+    else:  # 새로만드는건
+        if _HEADER == None:
+            print(">>> 헤더 리스트를 먼저 넣어주세요")
+            return None
+        book = Workbook()
+        sheet = book.active
+        sheet.title = '시트이름'
+        sheet.append(_HEADER)
+        sheet.column_dimensions['A'].width = 20
+        sheet.column_dimensions['B'].width = 20
+        sheet.column_dimensions['C'].width = 20
+        sheet.column_dimensions['D'].width = 20
+        sheet.column_dimensions['E'].width = 20
+        sheet.column_dimensions['F'].width = 40
+        book.save(_FILENAME)
+
 if __name__ == "__main__":
     # CODE HERE
     # driver setting
+    README()
     driver = webdriver.Chrome('./chromedriver')
     driver.maximize_window()
     driver.get('https://map.naver.com/')
     # get cookie
+    FILENAME = input(">>> 저장 파일 *이름만* 적어주세요 ( 확장자 미포함 ) :") + '.xlsx'
+    HEADER = ['키워드','업소명', '도로명주소', '지번주소', '전화번호', '홈페이지1', '상세페이지']
+    save_excel(FILENAME,None,HEADER) # init
+    
     while True:
-        keyword = input(">>> 검색하실 키워드를 입력(끝내려면 'quit' 입력) : ").strip()
-        if keyword == 'quit':
+        keyword = input(">>> 검색하실 키워드를 입력(끝내려면 0 입력) : ").strip()
+        if keyword == '0':
             break
         headers['cookie'] = get_cookie()
         headers['cookie'] = 'asd'
